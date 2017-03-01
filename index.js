bf90db1d1fc6f483533559abea3949326bc45dd0
Anthony At Zinid <anthony@zinid.com>
2017-02-28 22:22:37 -0600

    Testing / proof of concept

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('test')
+const Fontmin = require('fontmin')
+
+const fontmin = new Fontmin()
+	.src('fonts/*.ttf')
+	.use(Fontmin.glyph({
+		text: '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz%20~`!@#$%^&*()-_=+{}[]\\|;:\'"<,>./?©',
+		// keep ttf hint info (fpgm, prep, cvt). default = true
+		hinting: true
+	}))
+	.use(Fontmin.ttf2woff({
+		deflate: true // deflate woff
+	}))
+	.dest('build/fonts')
+
+fontmin.run((err, files) => {
+	if (err) {
+		throw err
+	}
+
+	console.log(files)
+})
